c40a17cd6ec3dd85fd159693e208144a68d510c1
Michael Truog <mjtruog@gmail.com>
2015-12-23 15:04:29 -0800

    Version 1.5.1.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CloudIServiceDbElasticsearch do
 
   def project do
     [app: :cloudi_service_db_elasticsearch,
-     version: "1.5.0",
+     version: "1.5.1",
      language: :erlang,
      description: description,
      package: package,
@@ -14,12 +14,12 @@ defmodule CloudIServiceDbElasticsearch do
     [# TODO, erlasticsearch dependencies cause problems here, need to get locked
      {:erlasticsearch,
       [git: "https://github.com/dieswaytoofast/erlasticsearch.git",
-       tag: "1.6.3"]},
+       tag: "1.6.8"]},
      {:poolboy, "~> 1.4.0", override: true}, # override for 17.x types
      # confirm dependencies due to erlasticsearch
-     {:reltool_util, "~> 1.5.0", override: true},
+     {:reltool_util, "~> 1.5.1", override: true},
      {:jsx, "~> 2.1.1", override: true},
-     {:cloudi_core, "~> 1.5.0"}]
+     {:cloudi_core, "~> 1.5.1"}]
   end
 
   defp description do
@@ -28,7 +28,7 @@ defmodule CloudIServiceDbElasticsearch do
 
   defp package do
     [files: ~w(src doc test rebar.config README.markdown),
-     contributors: ["Mahesh Paolini-Subramanya", "Michael Truog"],
+     maintainers: ["Mahesh Paolini-Subramanya", "Michael Truog"],
      licenses: ["BSD"],
      links: %{"Website" => "http://cloudi.org",
               "GitHub" => "https://github.com/CloudI/" <>
